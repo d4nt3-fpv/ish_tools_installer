70e9ff924c0a9337e39818e5969201ec93faad89
DroneDante <iPad18Dante@outlook.de>
2021-02-20 13:24:27 +0100

    First commit

---
+++ modules/system.py
@@ -0,0 +1,29 @@
+import json
+
+with open('core/data.json') as file:
+    data = json.load(file)
+
+
+
+
+# print(data["4nonimizer"]["name"])
+
+names = []
+urls = []
+dependencies = []
+categories = [[]]
+
+
+for name in data:
+    names.append(name)
+    urls.append(data[name]["url"])
+    dependencies.append(data[name]["dependency"])
+    categories.append([str(data[name]["name"]) , str(data[name]["category"])])
+
+
+print(names)
+print(urls)
+print(dependencies)
+print(categories)
+
+# print(data)
